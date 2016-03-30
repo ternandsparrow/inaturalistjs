5d719d89f8ded5b9431d6691833264686e79ba55
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2016-03-30 15:10:33 -0700

    Added Photo class.

---
+++ test/models/photo.js
@@ -0,0 +1,23 @@
+"use strict";
+var expect = require( "chai" ).expect,
+    Photo = require( "../../lib/models/photo" )
+
+describe( "Photo", function( ) {
+  describe( "photoUrl", function( ) {
+    it( "should return undefined if no photo present", function( ) {
+      let p = new Photo({ url: null });
+      expect( p.photoUrl( ) ).to.be.undefined;
+    } );
+    it( "takes optional sizes", function( ) {
+      let p = new Photo({
+        attribution: "no rights reserved",
+        id: 519,
+        license_code: "cc0",
+        url: "http://localhost:3000/attachments/local_photos/files/519/square/pic1.jpg"
+      });
+      expect( p.photoUrl( "medium" ) ).to.match( /medium/ );
+      expect( p.photoUrl( "large" ) ).to.match( /large/ );
+      expect( p.photoUrl( ) ).to.match( /square/ );
+    } );
+  });
+} );
