236b04de874c6b889709b6ed0f0cbdb0cd71d5cb
Patrick Leary <patrick.r.leary@gmail.com>
2016-04-07 13:43:00 -0400

    exposing models

--- browserify.js
+++ browserify.js
@@ -5,5 +5,11 @@ module.exports = {
   observationFieldValues: require( "./lib/endpoints/observation_field_values" ),
   photos: require( "./lib/endpoints/photos" ),
   projects: require( "./lib/endpoints/projects" ),
-  taxa: require( "./lib/endpoints/taxa" )
+  taxa: require( "./lib/endpoints/taxa" ),
+  Comment: require( "./lib/models/comment" ),
+  Identification: require( "./lib/models/identification" ),
+  Observation: require( "./lib/models/observation" ),
+  ObservationFieldValue: require( "./lib/models/observation_field_value" ),
+  Photo: require( "./lib/models/photo" ),
+  Taxon: require( "./lib/models/taxon" )
 };
