43e96eecd988de5ded2651ea77d31d08123ef248
Patrick Leary <patrick.r.leary@gmail.com>
2018-07-19 12:11:56 -0400

    add observation_photos delete endpoint; set Via header for inaturalistjs requests

---
+++ test/endpoints/observation_photos.js
@@ -0,0 +1,36 @@
+var expect = require( "chai" ).expect,
+    nock = require( "nock" ),
+    observation_photos = require( "../../lib/endpoints/observation_photos" );
+
+describe( "ObservationPhotos", function( ) {
+
+  describe( "create", function( ) {
+    it( "posts to /observation_photos", function( done ) {
+      nock( "http://localhost:3000" ).
+        post( "/observation_photos" ).
+        reply( 200, { id: 1 } );
+      observation_photos.create({ body: "testbody" }).then( function( ) {
+        done( );
+      });
+    });
+  });
+
+  describe( "delete", function( ) {
+    it( "deletes to /observation_photos", function( done ) {
+      nock( "http://localhost:3000" ).
+        delete( "/observation_photos/1?id=1", { id: 1 }).
+        reply( 200, { id: 1 } );
+      observation_photos.delete({ id: 1 }).then( function( ) {
+        done( );
+      });
+    });
+
+    it( "throws errors", function( done ) {
+      observation_photos.delete({ any: "thing" }).catch( function( e ) {
+        expect( e.message ).to.eq( "id required" );
+        done( );
+      });
+    });
+  });
+
+});
